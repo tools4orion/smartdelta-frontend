78291d5928c2d62001eb36978bb38c56f898a024
Alper Celik <alperwithsteel@gmail.com>
2024-12-20 11:16:01 +0300

    fix: getEncryptedVercelToken action totally removed on frontend

--- src/network/endpoints/vercel.js
+++ src/network/endpoints/vercel.js
@@ -3,9 +3,7 @@ import { axiosClient } from "../apiClient";
 const vercelEndpoints = {
   listIntegrations: () => axiosClient.get("/vercel/list"),
   saveIntegration: (data) => axiosClient.post("/vercel/store", data),
-  fetchProjects: (encryptedToken) =>
-    axiosClient.post("/vercel/projects", { encryptedToken }),
-  getEncryptedToken: (email) => axiosClient.post("/vercel/token", { email }),
+  fetchProjects: (email) => axiosClient.post("/vercel/projects", { email }),
 };
 
 export default vercelEndpoints;
